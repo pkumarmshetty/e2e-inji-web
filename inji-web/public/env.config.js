16f08a3b0aa3ff82eb5e90c6c45a0fbbe6a117a6
Kumar M <pkumarm@deloitte.com>
2025-02-26 19:38:20 +0530

    title change

--- inji-web/public/env.config.js
+++ inji-web/public/env.config.js
@@ -2,7 +2,7 @@ window._env_ = {
     DEFAULT_LANG: "en",
     DEFAULT_THEME: "",
     DEFAULT_FAVICON: "favicon.ico",
-    DEFAULT_TITLE: "Inji Wallet",
+    DEFAULT_TITLE: "T&T Web",
     DEFAULT_FONT_URL: "https://fonts.googleapis.com/css?family=Inter",
     MIMOTO_HOST: "https://vertically-wealthy-sheepdog.ngrok-free.app/inji/residentmobileapp"
 };
